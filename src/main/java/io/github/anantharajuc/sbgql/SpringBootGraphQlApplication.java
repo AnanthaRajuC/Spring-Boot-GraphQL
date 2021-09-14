25706884885277bd11ed76254b219706540f89bf
Anantha Raju C <arcswdev@gmail.com>
2021-09-14 11:52:54 +0530

    chore: ADDED profiles, banner, flyway .sql files

--- src/main/java/io/github/anantharajuc/sbgql/SpringBootGraphQlApplication.java
+++ src/main/java/io/github/anantharajuc/sbgql/SpringBootGraphQlApplication.java
@@ -3,11 +3,17 @@ package io.github.anantharajuc.sbgql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot GraphQL.
+ *
+ * @author <a href="mailto:arcswdev@gmail.com">Anantha Raju C</a>
+ *
+ */
 @SpringBootApplication
-public class SpringBootGraphQlApplication {
-
-	public static void main(String[] args) {
+public class SpringBootGraphQlApplication 
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(SpringBootGraphQlApplication.class, args);
 	}
-
 }
---
+++ src/main/resources/db/migration/mysql/V0_0_1__SBGQL_structure_basic.sql
@@ -0,0 +1,19 @@
+/*
+ * Engine: MySQL
+ * Version: 0.0.1
+ * Description: Initial database structure
+ */
+
+/*
+-- create schema
+CREATE SCHEMA [schema_name];
+
+-- use schema
+USE [schema_name];
+
+-- Create user 
+create user 'USERNAME'@'IPADDRESS' identified by 'PASSWORD';
+
+-- Grant privileges to user
+grant all privileges on *.* to 'USERNAME'@'IPADDRESS' with grant option;
+ */
