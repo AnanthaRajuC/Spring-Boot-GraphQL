4c912bfacbda1623197be25afedd126b5e511100
Anantha Raju C <arcswdev@gmail.com>
2021-09-14 11:14:17 +0530

    Initial Commit

---
+++ src/test/java/io/github/anantharajuc/sbgql/SpringBootGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.anantharajuc.sbgql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
